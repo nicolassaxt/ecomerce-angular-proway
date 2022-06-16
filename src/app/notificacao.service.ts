99fbbaad828b9786219b29d7aa89ade655fa45ab
Nicolas Brasiliano <nicolassaxt@gmail.com>
2022-06-16 15:28:57 -0300

    build

---
+++ src/app/notificacao.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificacaoService {
+
+  constructor(
+    private snakBar: MatSnackBar
+  ) { }
+
+  notificar(mensagem: string){
+    this.snakBar.open(mensagem, "OK",{
+      duration: 2000,
+      verticalPosition: "top",
+      horizontalPosition: "center"
+    });
+  }
+}
